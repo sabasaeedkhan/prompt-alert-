119e641fc92887773ad008a96d5fa459bf34fca0
Saba Saeed khan <144582707+saeedsamina@users.noreply.github.com>
2023-11-22 20:44:40 +0500

    check paragraph length

---
+++ scrpt.mjs
@@ -0,0 +1,7 @@
+
+ 
+ let paragraph = prompt("Tell me about yourself");
+ console.log("Length of your paragraph is " + paragraph.length)
+ let words=paragraph.split(" ");
+ console.log(paragraph.length);
+ console.log(words);
